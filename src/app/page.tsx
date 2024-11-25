26072372e14e1cdc8e5c74d7085247095f86ed39
Souvik <souvik13109@gmail.com>
2024-11-25 11:38:49 +0530

    FAQ Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import AboutUs from "@/app/about-us/page";
 import Hero from "@/app/hero/page";
+import Faq from "./faq/page";
 
 export default function Home() {
   return (
       <>
         <Hero/>
         <AboutUs/>
+        <Faq/>
       </>
   )
 }
