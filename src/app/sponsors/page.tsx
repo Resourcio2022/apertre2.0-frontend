4456ab9a589228865f5ea91cfb8f418ef20ab6c9
SumanJain2005 <sumannareshclg@gmail.com>
2024-11-27 19:00:44 +0530

    sponsor.constants.ts file added

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -1,16 +1,7 @@
 'use client'
 
-interface Sponsor {
-    id: number;
-    name: string;
-    image?: string;
-    tier: string;
-}
+import { sponsors, sponsorTiers, Sponsor, SponsorTier } from '../../constants/sponsor.constants';
 
-interface SponsorTier {
-    name: string;
-    color: string;
-}
 const PlaceholderSVG = () => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -31,24 +22,6 @@ const PlaceholderSVG = () => (
         </text>
     </svg>
 );
-const sponsorTiers: SponsorTier[] = [
-    { name: "PLATINUM SPONSORS", color: "text-gray-200" },
-    { name: "GOLD SPONSORS", color: "text-yellow-500" },
-    { name: "SILVER SPONSORS", color: "text-gray-400" },
-    { name: "BRONZE SPONSORS", color: "text-amber-700" }
-];
-
-const sponsors: Sponsor[] = [
-    { id: 1, name: "Platinum Sponsor 1", tier: "PLATINUM SPONSORS" },
-    { id: 2, name: "Platinum Sponsor 2", tier: "PLATINUM SPONSORS" },
-    { id: 3, name: "Gold Sponsor 1", tier: "GOLD SPONSORS" },
-    { id: 4, name: "Silver Sponsor 1", tier: "SILVER SPONSORS" },
-    { id: 5, name: "Bronze Sponsor 1", tier: "BRONZE SPONSORS" },
-    { id: 6, name: "Platinum Sponsor 3", tier: "PLATINUM SPONSORS" },
-    { id: 7, name: "Gold Sponsor 2", tier: "GOLD SPONSORS" },
-    { id: 8, name: "Silver Sponsor 2", tier: "SILVER SPONSORS" },
-    { id: 9, name: "Bronze Sponsor 2", tier: "BRONZE SPONSORS" }
-];
 
 export default function SponsorsSection() {
     return (
