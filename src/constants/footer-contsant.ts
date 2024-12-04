3c5e6541da4c7b69d99fa4847518fb0f87387592
IndraniSom <indranisom39@gmail.com>
2024-12-04 17:24:24 +0530

    added footer

---
+++ src/constants/footer-contsant.ts
@@ -0,0 +1,17 @@
+interface FooterLink {
+    title: string;
+    href: string;
+  }
+  
+  export const footerLinks: FooterLink[] = [
+    { title: "Brand Assets", href: "" },
+    { title: "Rules and Guidelines", href: "" },
+    { title: "Code of Conduct", href: "" },
+  ];
+  
+  export const supportLinks: FooterLink[] = [
+    { title: "Support", href: "" },
+    { title: "Privacy Policy", href: "" },
+    { title: "Terms of Use", href: "" },
+    { title: "Contact Us", href: "" },
+  ];
