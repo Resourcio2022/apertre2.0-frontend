bb136d3ebc497a3c6261eb1d9421ec425334c04b
IndraniSom <indranisom39@gmail.com>
2024-12-04 17:36:08 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 
 import AboutUs from "@/app/_components/about-us";
 import Hero from "@/app/_components/hero-section";
-
+import FAQ from "@/app/_components/faq";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
+      <Navbar/>
       <Hero />
       <AboutUs />
+      <FAQ/>
+      <Footer/>
     </>
   )
 }
