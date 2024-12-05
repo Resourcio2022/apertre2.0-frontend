f31db156383dfa457bc506522d284a7af2632a41
IndraniSom <indranisom39@gmail.com>
2024-12-05 11:20:07 +0530

    testimonials done

--- src/app/_components/testimonials.tsx
+++ src/app/_components/testimonials.tsx
@@ -1,42 +1,8 @@
 import React from 'react'
 import {InfiniteMovingCards} from '@/components/InfiniteMovingCards'
+import {testimonialsdata} from '@/constants/testimonialsconstant'
 const testimonials = () => {
-    const testimonials = [
-        {
-          quote:
-            "Being part of Apertre as a project admin and speaker was an incredible experience. The event provided a fantastic platform to share insights",
-          name: "-Bishakh Neogi",
-          title: "",
-          pic:'/bisakh.png'
-        },
-        {
-          quote:
-            "It was an absolute honor to be a speaker at Apertre last year! Huge kudos to the organizing team for putting together such a seamless and impactful event.",
-          name: "-Arnab Chatterjee",
-          title: "",
-          pic:'/arnab.jpeg'
-        },
-        {
-          quote: "I was truly honored to be a part of Apertre as a speaker last year! A big shoutout to the organizers for creating such a well-executed and meaningful event.",
-          name: "-Ronit Banerjee",
-          title: "",
-          pic:'/ronit.jpeg'
-        },
-        {
-          quote:
-            "It was very nice to be a part of such a grand event...may Reasourcio Community soar to great heights!",
-          name: "-Sayak Sengupta",
-          title: "",
-          pic:'/sayak.jpeg'
-        },
-        {
-          quote:
-            "It was great being a part of this amazing event! Kudos to the team for successfully organising it smoothly",
-          name: "-Harshavardhan Bajoria",
-          title: "",
-          pic:"https://utfs.io/f/SC1fS7AGgsX7BdSvoJ18Q2Z9yJ6Kbv5HGOWEMaS7ulNqm4s0"
-        },
-      ];
+    
   return (
     <div className='flex w-full h-full flex-col bg-customblack md:p-20 p-10 '>
         <span className="text-nowrap text-2xl md:text-6xl font-mokoto text-textyellow text-left mb-10">
@@ -44,7 +10,7 @@ const testimonials = () => {
       </span>
         <div className="h-full w-full rounded-md flex flex-col relative overflow-hidden">
       <InfiniteMovingCards
-        items={testimonials}
+        items={testimonialsdata}
         direction="right"
         speed="slow"
       />
