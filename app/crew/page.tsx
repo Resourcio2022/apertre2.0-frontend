5fae3e7381cf768b4ad6074c15896d1423abce89
SumanJain2005 <sumannareshclg@gmail.com>
2024-12-13 13:58:56 +0530

    added the coming soon gif

---
+++ app/crew/page.tsx
@@ -0,0 +1,9 @@
+
+import RocketComingSoon from "@/components/ComingSoon";
+export default function page() {
+    return (
+        <>
+            <RocketComingSoon />
+        </>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import FAQ from "./_components/faq";
 import Hero from "./_components/hero-section";
 import Registration from "./_components/registration";
 import Testimonials from "./_components/testimonials";
-
+import RocketComingSoon from "@/components/ComingSoon";
 export default function Home() {
   return (
     <>
@@ -11,6 +11,7 @@ export default function Home() {
       <AboutUs />
       <Registration />
       {/* <Testimonials /> */}
+      
       <FAQ />
     </>
   )
