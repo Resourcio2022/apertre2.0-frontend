d40d7935978baf5ffb1684ef5de6ccb85bddfcdb
ABHIRUP <abhirupsaha02@gmail.com>
2024-12-15 23:52:17 +0530

    star beside required fields

--- app/(registration)/community-partner/page.tsx
+++ app/(registration)/community-partner/page.tsx
@@ -9,19 +9,19 @@ const page = () => {
   const inputGroups = [
     {
       fields: [
-        { name: "firstName", type: "text", placeholder: "First name", required: true },
-        { name: "lastName", type: "text", placeholder: "Last name", required: true },
+        { name: "firstName", type: "text", placeholder: "First name*", required: true },
+        { name: "lastName", type: "text", placeholder: "Last name*", required: true },
       ] as InputField[],
     },
     {
       fields: [
-        { name: "phoneNumber", type: "tel", placeholder: "Phone number", required: true },
-        { name: "discordUsername", type: "text", placeholder: "Discord username", required: true },
+        { name: "phoneNumber", type: "tel", placeholder: "Phone number*", required: true },
+        { name: "discordUsername", type: "text", placeholder: "Discord username*", required: true },
       ] as InputField[],
     },
     {
       fields: [
-        { name: "address", type: "text", placeholder: "Address", required: true },
+        { name: "address", type: "text", placeholder: "Address*", required: true },
       ] as InputField[]
     }
   ]
@@ -29,24 +29,24 @@ const page = () => {
   const additionalInputGroups = [
     {
       fields: [
-        { name: "communityName", type: "text", placeholder: "Community name", required: true },
-        { name: "communityStrength", type: "text", placeholder: "Community strength", required: true },
+        { name: "communityName", type: "text", placeholder: "Community name*", required: true },
+        { name: "communityStrength", type: "text", placeholder: "Community strength*", required: true },
       ] as InputField[],
     },
     {
       fields: [
-        { name: "linkedinUrl", type: "url", placeholder: "LinkedIn profile URL", required: true },
+        { name: "linkedinUrl", type: "url", placeholder: "LinkedIn profile URL*", required: true },
       ] as InputField[],
     },
     {
       fields: [
-        { name: "instagramUsername", type: "text", placeholder: "Instagram username", required: true },
+        { name: "instagramUsername", type: "text", placeholder: "Instagram username*", required: true },
         { name: "twitterUsername", type: "text", placeholder: "Twitter username", required: false }
       ] as InputField[],
     },
     {
       fields: [
-        { name: "communityUrl", type: "url", placeholder: "Community website URL", required: true },
+        { name: "communityUrl", type: "url", placeholder: "Community website URL*", required: true },
       ] as InputField[],
     },
     {
