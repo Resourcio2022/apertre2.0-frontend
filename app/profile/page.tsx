568f58eec9fc5b9c4818732bc37827051ff0d291
IndraniSom <indranisom39@gmail.com>
2025-01-21 23:58:38 +0530

    added profiles

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,15 +2,21 @@
 
 import { useGitHub } from "@/hooks/useGithubUser"
 import { useUser } from "@clerk/clerk-react"
-
+import Mentee from "./_components/mentee"
+import CommunityPartner from "./_components/community-partner";
+import Evangelist from "./_components/evangelist";
+import Mentor from "./_components/mentor";
 export default function page() {
     // const { user } = useUser()
-    // const { githubUsername } = useGitHub()
+    const { githubUsername } = useGitHub();
+
     // console.log(user?.publicMetadata)
 
 
     return (
-        <>
-        </>
+    //    <><Mentee username={githubUsername} /></>
+    // <><CommunityPartner username={githubUsername}/></>
+    // <><Evangelist username={githubUsername}/></>
+    <><Mentor username={githubUsername}/></>
     )
 }
--- hooks/useGithubUser.ts
+++ hooks/useGithubUser.ts
@@ -9,20 +9,21 @@ export const useGitHub = () => {
   const [githubUsername, setGitHubUsername] = useState<string>();
   const [email, setEmail] = useState<string>();
   const [clerk_userId, setClerk_userId] = useState<string>();
-
+  const [image,setimage]=useState<string>();
   useEffect(() => {
     const fetchGitHubUsername = () => {
       if (isLoaded && isSignedIn && user) {
         const githubUsername = user.username
         const emailAddress = user.primaryEmailAddress?.emailAddress
         const user_Id = user.id
-
-        if (!githubUsername || !emailAddress) {
+        const image=user.imageUrl
+        if (!githubUsername || !emailAddress || !image) {
           return
         }
         setGitHubUsername(githubUsername);
         setEmail(emailAddress);
-        setClerk_userId(user_Id)
+        setClerk_userId(user_Id);
+        setimage(image);
       }
       setLoading(false);
     };
@@ -30,5 +31,5 @@ export const useGitHub = () => {
     fetchGitHubUsername();
   }, [isLoaded, isSignedIn, user]);
 
-  return { githubUsername, email, clerk_userId, loading, isSignedIn };
+  return { githubUsername, email, clerk_userId, loading, isSignedIn,image };
 };
