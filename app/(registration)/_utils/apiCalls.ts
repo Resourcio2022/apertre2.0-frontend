e6139ec1b681eed363847c08207f6b2d90e76918
ABHIRUP <abhirupsaha02@gmail.com>
2025-01-21 15:36:26 +0530

    mentee registration final checks

--- app/(registration)/_utils/apiCalls.ts
+++ app/(registration)/_utils/apiCalls.ts
@@ -3,163 +3,146 @@ export type Role = 'mentee' | 'mentor' | 'evangelist' | 'community partner' | 'p
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function evangelistSignup(clerk_userId: string, role: Role, email: string, username: string, fullname: string, address: string, phoneNumber: string, linkedinUrl: string, instagramUsername: string, discordUsername: string, twitterUsername: string | undefined, collegeName: string): Promise<string> {
-  const payload = {
-    clerk_userId,
-    role,
-    email,
-    username,
-    fullname,
-    address,
-    phoneNumber,
-    linkedinUrl,
-    instagramUsername,
-    discordUsername,
-    collegeName
-  }
-
-  if (twitterUsername !== "") {
-    Object.assign(payload, { twitterUsername })
-  }
-
-  const res = await fetch(`${API_URL}/evangelist`, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: {
-      "Content-Type": "application/json"
+    const payload = {
+        clerk_userId,
+        role,
+        email,
+        username,
+        fullname,
+        address,
+        phoneNumber,
+        linkedinUrl,
+        instagramUsername,
+        discordUsername,
+        collegeName
     }
-  })
-  const data = await res.json()
 
-  if (!res.ok) {
-    throw new Error(data.message)
-  }
+    if (twitterUsername !== "") {
+        Object.assign(payload, { twitterUsername })
+    }
 
-  return data.message;
+    const res = await fetch(`${API_URL}/evangelist`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(data.message)
+    }
+
+    return data.message;
 }
 
 export async function communityPartnerSignup(clerk_userId: string, role: Role, email: string, username: string, fullname: string, address: string, phoneNumber: string, linkedinUrl: string, instagramUsername: string, discordUsername: string, twitterUsername: string | undefined, communityName: string, communityUrl: string, communityStrength: number) {
-  const payload = {
-    clerk_userId,
-    role,
-    email,
-    username,
-    fullname,
-    address,
-    phoneNumber,
-    linkedinUrl,
-    instagramUsername,
-    discordUsername,
-    communityName,
-    communityUrl,
-    communityStrength
-  }
-
-  if (twitterUsername !== "") {
-    Object.assign(payload, { twitterUsername })
-  }
-
-  const res = await fetch(`${API_URL}/community-partner`, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: {
-      "Content-Type": "application/json"
+    const payload = {
+        clerk_userId,
+        role,
+        email,
+        username,
+        fullname,
+        address,
+        phoneNumber,
+        linkedinUrl,
+        instagramUsername,
+        discordUsername,
+        communityName,
+        communityUrl,
+        communityStrength
     }
-  })
-  const data = await res.json()
 
-  if (!res.ok) {
-    throw new Error(data.message)
-  }
+    if (twitterUsername !== "") {
+        Object.assign(payload, { twitterUsername })
+    }
 
-  return data.message;
+    const res = await fetch(`${API_URL}/community-partner`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(data.message)
+    }
+
+    return data.message;
 }
 
-export const mentorSignup = async (
-  clerk_userId: string,
-  role: string,
-  email: string,
-  username: string,
-  fullname: string,
-  address: string,
-  phoneNumber: string,
-  discordUsername: string,
-  linkedinUrl: string,
-  twitterUsername: string,
-  techstack: string[]
-) => {
-  const payload = {
-    clerk_userId,
-    role,
-    email,
-    username,
-    fullname,
-    address,
-    phoneNumber,
-    discordUsername,
-    linkedinUrl,
-    techstack
-  }
-  if (twitterUsername !== "") {
-    Object.assign(payload, { twitterUsername })
-  }
-  const res = await fetch(`${API_URL}/mentor`, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-    headers: {
-      "Content-Type": "application/json"
+export const mentorSignup = async (clerk_userId: string, role: Role, email: string, username: string, fullname: string, address: string, phoneNumber: string, discordUsername: string, linkedinUrl: string, twitterUsername: string, techstack: string[]) => {
+    const payload = {
+        clerk_userId,
+        role,
+        email,
+        username,
+        fullname,
+        address,
+        phoneNumber,
+        discordUsername,
+        linkedinUrl,
+        techstack
     }
-  })
-  const data = await res.json()
 
-  if (!res.ok) {
-    throw new Error(data.message)
-  }
+    if (twitterUsername !== "") {
+        Object.assign(payload, { twitterUsername })
+    }
 
-  return data.message;
+    const res = await fetch(`${API_URL}/mentor`, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const data = await res.json()
 
+    if (!res.ok) {
+        throw new Error(data.message)
+    }
+
+    return data.message;
 }
 
-export const menteeSignup = async (
-  clerk_userId: string,
-  role: string,
-  email: string,
-  username: string,
-  fullname: string,
-  address: string,
-  phoneNumber: string,
-  linkedinUrl: string,
-  discordUsername: string,
-  twitterUsername: string,
-  referralCode: string,
-) => {
-  const payload = {
-    clerk_userId,
-    role,
-    email,
-    username,
-    fullname,
-    address,
-    phoneNumber,
-    linkedinUrl,
-    discordUsername,
-    referralCode
-  }
-
-  if (twitterUsername !== "" && referralCode !== "") {
-    Object.assign(payload, { twitterUsername, referralCode })
-  }
-  const res = await fetch(`${API_URL}/participants`, {
-    method: "POST",
-    body: JSON.stringify(payload),
-    headers: {
-      "Content-Type": "application/json"
+export const menteeSignup = async (clerk_userId: string, role: string, email: string, username: string, fullname: string, address: string, phoneNumber: string, linkedinUrl: string, discordUsername: string, twitterUsername: string, referralCode: string) => {
+    const payload = {
+        clerk_userId,
+        role,
+        email,
+        username,
+        fullname,
+        address,
+        phoneNumber,
+        linkedinUrl,
+        discordUsername,
+        referralCode
     }
-  })
-  const data = await res.json()
 
-  if (!res.ok) {
-    throw new Error(data.message)
-  }
-  return data
+    if (twitterUsername !== "") {
+        Object.assign(payload, { twitterUsername })
+    }
+
+    if (referralCode !== "") {
+        Object.assign(payload, { referralCode })
+    }
+    
+    const res = await fetch(`${API_URL}/participant`, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(data.message)
+    }
+
+    return data.message
 }
 
