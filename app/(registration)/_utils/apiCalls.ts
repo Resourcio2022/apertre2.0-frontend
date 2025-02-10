803083ee47b849b3e8c6ad0723d9c986bddc0971
ABHIRUP <abhirupsaha02@gmail.com>
2025-02-10 13:37:50 +0530

    idk-2

--- app/(registration)/_utils/apiCalls.ts
+++ app/(registration)/_utils/apiCalls.ts
@@ -2,6 +2,14 @@ export type Role = 'mentee' | 'mentor' | 'evangelist' | 'community partner' | 'p
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+interface Repo {
+    repoName: string;
+    repoURL: string;
+    description: string;
+    projectDomain: string;
+    techStack: string[];
+}
+
 export async function evangelistSignup(clerk_userId: string, role: Role, email: string, username: string, fullname: string, address: string, phoneNumber: string, linkedinUrl: string, instagramUsername: string, discordUsername: string, twitterUsername: string | undefined, collegeName: string): Promise<string> {
     const payload = {
         clerk_userId,
@@ -74,7 +82,7 @@ export async function communityPartnerSignup(clerk_userId: string, role: Role, e
     return data.message as string;
 }
 
-export const mentorSignup = async (clerk_userId: string, role: Role, email: string, username: string, fullname: string, address: string, phoneNumber: string, discordUsername: string, linkedinUrl: string, twitterUsername: string, techstack: string[]) => {
+export async function mentorSignup(clerk_userId: string, role: Role, email: string, username: string, fullname: string, address: string, phoneNumber: string, discordUsername: string, linkedinUrl: string, twitterUsername: string, techstack: string[]) {
     const payload = {
         clerk_userId,
         role,
@@ -108,7 +116,7 @@ export const mentorSignup = async (clerk_userId: string, role: Role, email: stri
     return data.message as string;
 }
 
-export const menteeSignup = async (clerk_userId: string, role: string, email: string, username: string, fullname: string, address: string, phoneNumber: string, linkedinUrl: string, discordUsername: string, twitterUsername: string, referralCode: string) => {
+export async function menteeSignup(clerk_userId: string, role: string, email: string, username: string, fullname: string, address: string, phoneNumber: string, linkedinUrl: string, discordUsername: string, twitterUsername: string, referralCode: string) {
     const payload = {
         clerk_userId,
         role,
@@ -129,7 +137,7 @@ export const menteeSignup = async (clerk_userId: string, role: string, email: st
     if (referralCode !== "") {
         Object.assign(payload, { referralCode })
     }
-    
+
     const res = await fetch(`${API_URL}/participant`, {
         method: "POST",
         body: JSON.stringify(payload),
@@ -146,24 +154,58 @@ export const menteeSignup = async (clerk_userId: string, role: string, email: st
     return data.message as string;
 }
 
-export async function getTechStacks() { 
+export async function projectAdminSignup(clerk_userId: string, role: Role, email: string, username: string, fullname: string, address: string, phoneNumber: string, discordUsername: string, linkedinUrl: string, twitterUsername: string, repos: Repo[]) {
+    const payload = {
+        clerk_userId,
+        role,
+        email,
+        username,
+        fullname,
+        address,
+        phoneNumber,
+        linkedinUrl,
+        discordUsername,
+        repos
+    }
+
+    if (twitterUsername !== "") {
+        Object.assign(payload, { twitterUsername })
+    }
+
+    const res = await fetch(`${API_URL}/project-admin`, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw new Error(data.message)
+    }
+
+    return data.message as string;
+}
+
+export async function getTechStacks() {
     const res = await fetch(`${API_URL}/common/techstack`)
 
     if (!res.ok) {
         return {}
     }
     const data = await res.json()
-    
+
     return data as Record<string, string>
 }
 
-export async function getProjectDomains() { 
+export async function getProjectDomains() {
     const res = await fetch(`${API_URL}/common/project-domain`)
 
     if (!res.ok) {
         return {}
     }
     const data = await res.json()
-    
+
     return data as Record<string, string>
 }
