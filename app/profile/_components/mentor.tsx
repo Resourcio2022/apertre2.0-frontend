5c61e2469231020849ab242a2545a2b1c932470b
ABHIRUP <abhirupsaha02@gmail.com>
2025-02-16 00:02:15 +0530

    type handling just better

--- app/profile/_components/mentor.tsx
+++ app/profile/_components/mentor.tsx
@@ -48,7 +48,7 @@ const Mentor = memo(function Mentor({ username, image }: MentorProps) {
 			</div>
 		)
 	}
-	else if(!profile) return null;
+	else if (!profile) return null;
 
 	return (
 		<div className="flex flex-col justify-center text-textyellow">
@@ -65,14 +65,12 @@ const Mentor = memo(function Mentor({ username, image }: MentorProps) {
 				<div className="flex flex-col justify-between items-baseline">
 					<div className="font-mokoto font-normal text-2xl text-nowrap flex flex-col md:flex-row">
 						<span className="text-white whitespace-nowrap">{'>'}_.. Hello&nbsp;</span>
-						{profile?.fullname &&
-							<TypewriterEffectSmooth words={profile.fullname.split(" ").map(word => ({ text: word }))} />
-						}
+						<TypewriterEffectSmooth words={profile.fullname.split(" ").map(word => ({ text: word }))} />
 					</div>
 					<div className="mt-4">
 						<h3 className="text-lg font-semibold text-white">Tech Stacks</h3>
 						<div className="flex flex-wrap gap-2 mt-2">
-							{profile?.techstack.map((stack, index) => (
+							{profile.techstack.map((stack, index) => (
 								<span key={index} className="bg-footer p-2 rounded">
 									{stack}
 								</span>
@@ -82,29 +80,25 @@ const Mentor = memo(function Mentor({ username, image }: MentorProps) {
 					<div className="flex flex-col gap-2 py-3 mb-4 text-white">
 						<div className="flex gap-3 items-center">
 							<MdAlternateEmail className="size-6 text-textyellow" />
-							<span className="font-Poppins font-normal">{profile?.email}</span>
+							<span className="font-Poppins font-normal">{profile.email}</span>
 						</div>
 						<div className="flex gap-3 items-center">
 							<BsPhone className="size-6 text-textyellow" />
-							<span className="font-Poppins font-normal">{profile?.phoneNumber}</span>
+							<span className="font-Poppins font-normal">{profile.phoneNumber}</span>
 						</div>
 						<div className="flex gap-3 items-center">
 							<FaHome className="size-6 text-textyellow" />
-							<span className="font-Poppins font-normal">{profile?.address}</span>
+							<span className="font-Poppins font-normal">{profile.address}</span>
 						</div>
 					</div>
 					<div className="w-full flex gap-5 justify-center md:justify-start text-white">
-						{profile && (
-							<Link href={`https://github.com/${profile.username}`} target="_blank">
-								<FaGithub className="size-6 hover:text-textyellow transition-colors duration-150" />
-							</Link>
-						)}
-						{profile && (
-							<Link href={profile.linkedinUrl} target="_blank">
-								<FaLinkedin className="size-6 hover:text-textyellow transition-colors duration-150" />
-							</Link>
-						)}
-						{profile && profile.twitterUsername && (
+						<Link href={`https://github.com/${profile.username}`} target="_blank">
+							<FaGithub className="size-6 hover:text-textyellow transition-colors duration-150" />
+						</Link>
+						<Link href={profile.linkedinUrl} target="_blank">
+							<FaLinkedin className="size-6 hover:text-textyellow transition-colors duration-150" />
+						</Link>
+						{profile.twitterUsername && (
 							<Link href={`https://x.com/${profile.twitterUsername}`} target="_blank">
 								<FaTwitter className="size-6 hover:text-textyellow transition-colors duration-150" />
 							</Link>
