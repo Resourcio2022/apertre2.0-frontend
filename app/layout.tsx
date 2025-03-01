2ca542ae7e97795f1154799d773f18192cced6e3
Sayan67 <sayan.das.21@aot.edu.in>
2025-03-01 20:53:05 +0530

    add: sitemap, metatag

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,12 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <meta
+            name="google-site-verification"
+            content="Hhtof5mJ4sQkvIGLkZDgPdA0NXUCIGZOhTSJDDkxAJI"
+          />
+        </head>
         <body
           className={`${mokotoVF.variable} ${poppins.className} antialiased`}
         >
