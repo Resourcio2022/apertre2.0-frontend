5d173f5263531424e89bcc088294e59b717c0707
ABHIRUP <abhirupsaha02@gmail.com>
2025-03-03 15:45:34 +0530

    ticket final

--- components/Ticket.tsx
+++ components/Ticket.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Role } from "@/app/(registration)/_utils/apiCalls";
-import { Download, DownloadIcon } from "lucide-react";
+import { DownloadIcon } from "lucide-react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
