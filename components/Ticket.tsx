872212afb04781c685e7af3cbac8a39c8e3aca78
ABHIRUP <abhirupsaha02@gmail.com>
2025-03-04 16:38:50 +0530

    blur while ticket loading

--- components/Ticket.tsx
+++ components/Ticket.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Role } from "@/app/(registration)/_utils/apiCalls";
+import { rgbDataURL } from "@/lib/blurryImage";
 import { DownloadIcon } from "lucide-react";
 import Head from "next/head";
 import Image from "next/image";
@@ -25,9 +26,7 @@ export default function Ticket({ name, github, role }: TicketProps) {
 
   useEffect(() => {
     function handleTicketGenerate() {
-      const ticketUrl = `/api/ticket?name=${encodeURIComponent(
-        name
-      )}&github=${encodeURIComponent(github)}&role=${encodeURIComponent(role)}`;
+      const ticketUrl = `/api/ticket?name=${encodeURIComponent(name)}&github=${encodeURIComponent(github)}&role=${encodeURIComponent(role)}`;
       setImg(ticketUrl);
     }
     handleTicketGenerate();
@@ -66,12 +65,12 @@ export default function Ticket({ name, github, role }: TicketProps) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Apertre2.0" />
         <meta property="og:description" content="Apertre2.0 Ticket" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
         <meta
           property="og:image"
           content={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/ticket?name=${name}&github=${github}`}
         />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <div className="flex flex-col items-startlg:items-end text-white">
         <h1 className="text-2xl font-mokoto text-customYellow">Your Ticket : </h1>
@@ -83,7 +82,7 @@ export default function Ticket({ name, github, role }: TicketProps) {
             height={1080}
             className="my-4 rounded-xl w-[500px]"
             loading="lazy"
-            blurDataURL=""
+            blurDataURL={rgbDataURL(128, 128, 128)}
           />
         )}
 
